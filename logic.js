895e8993e3eb33451329492f889867af6293c144
Kartikey Pathak <kartikeypathak08@gmail.com>
2025-02-19 21:56:24 +0530

    Update logic.js for product page

--- logic.js
+++ logic.js
@@ -15,3 +15,28 @@ btn.addEventListener("click",()=>{
     isbar=false;
 }
 })
+
+
+//Logic For The Jordan-1.html page
+let btn1=document.querySelector("#product-btn");
+let toggle=document.querySelector("#product");
+let close=document.querySelector("#close-btn");
+
+let is=false;
+let isclose=true;
+btn1.addEventListener("click",()=>{
+    if(!is){
+      toggle.classList.remove('hidden');
+       is=true;
+       isclose=false;
+    }else{
+        is=false;
+        isclose=true;
+    }
+
+})
+close.addEventListener("click",()=>{
+    if(!isclose){
+    toggle.classList.add('hidden');
+   }
+})
